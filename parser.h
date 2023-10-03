307f55cb1a7124e7f2584f14964cce3db727d284
Grant Orndorff <grant.orndorff@canonical.com>
2023-10-03 12:06:55 -0400

    minor fixups

--- parser.h
+++ parser.h
@@ -9,7 +9,6 @@
 #endif
 #define PARSE_TREE_PRINT(...) if (PRINT_PARSE_TREE) printf(__VA_ARGS__)
 #define PARSE_TREE_INDENT(n) for (int i = 0; i < (n); i++) { PARSE_TREE_PRINT("  "); }
-#define PARSE_TREE_PRINT_TOKEN_TYPE(token_type) PARSE_TREE_PRINT("%s", token_type_to_static_string(token_type))
 
 void parse(Token *tokens, int token_num, SymbolTable *symbols);
 
